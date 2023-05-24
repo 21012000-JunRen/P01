1ebf91fd339a1dbe10b4e040bbd2206c30372f86
CHUA JUN REN RYAIN <21012000@myrp.edu.sg>
2023-05-24 15:24:26 +0800

    updated push to git

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -15,4 +15,8 @@ public int multiply (int a, int b) {
 public int divide (int a, int b) {
 	return a / b;
 }
+
+public int triangle(int a, int b) {
+	return (a * b)/2;
+}
 }
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -24,10 +26,10 @@ public class CalculatorTest {
 	public void tearDown() throws Exception {
 	}
 	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 
 	@Test
 	public void testSubtract() {
@@ -76,6 +78,29 @@ public class CalculatorTest {
 		int actual = cal.divide(a, b);
 		int expected = 10;
 		assertEquals (expected, actual);
+	}
+	
+	@Test
+	public void testTriangle() {
+		//fail("Not yet implemented");
+		int a = 3;
+		int b = 6;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.triangle(a, b);
+		int expected = 9;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testTrue() {
+		//fail("Not yet implemented");
+		ArrayList<String> student = new ArrayList<String>();
+		student.add("Ryain");
+		student.add("darnest");
+		student.add("Trevor");
+		
+		assertTrue(student.contains("Ryain"));
 	}
 	
 
